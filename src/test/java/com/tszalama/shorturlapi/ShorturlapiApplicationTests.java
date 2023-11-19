50b6b86ce0c732a3b9a6441fb667e51ce8150f2d
tszalama <96135418+tszalama@users.noreply.github.com>
2023-11-19 16:29:11 +0200

    initial project structure

---
+++ src/test/java/com/tszalama/shorturlapi/ShorturlapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tszalama.shorturlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorturlapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
