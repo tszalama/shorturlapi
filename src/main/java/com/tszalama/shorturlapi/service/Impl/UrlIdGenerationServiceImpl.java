4d6bed9c1276595ed66ee1594720de15e221b381
tszalama <96135418+tszalama@users.noreply.github.com>
2023-11-19 19:38:19 +0200

    added more advanced id generation + integration test

--- src/main/java/com/tszalama/shorturlapi/service/Impl/UrlIdGenerationServiceImpl.java
+++ src/main/java/com/tszalama/shorturlapi/service/Impl/UrlIdGenerationServiceImpl.java
@@ -9,7 +9,7 @@ import java.util.Random;
 
 @Service
 public class UrlIdGenerationServiceImpl implements UrlIDGeneratorService {
-    private ShortUrlRepository shortUrlRepository;
+    private final ShortUrlRepository shortUrlRepository;
 
     @Autowired
     public UrlIdGenerationServiceImpl(ShortUrlRepository shortUrlRepository) {
@@ -19,6 +19,7 @@ public class UrlIdGenerationServiceImpl implements UrlIDGeneratorService {
     @Override
     public String getUniqueUrlId() {
         String urlId = generateId();
+        //In the case of an id collision, regenerate the id until the collision is avoided
         while (shortUrlRepository.existsById(urlId)) {
             urlId = generateId();
         }
@@ -26,10 +27,16 @@ public class UrlIdGenerationServiceImpl implements UrlIDGeneratorService {
     }
 
     private final Random random = new Random();
+    private final String USABLE_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     private String generateId() {
-        final int max = 2147483647;
-        final int min = 1;
-        final int randomId = random.nextInt(max - min) + min;
-        return String.valueOf(randomId);
+        StringBuilder result = new StringBuilder();
+        //Select random char from string and append it to the final id until we have an id of length 8 (62^8 combinations)
+        int limit = 8;
+        for (int i = 0; i < limit; i++) {
+            int index = random.nextInt(USABLE_CHARS.length());
+            result.append(USABLE_CHARS.charAt(index));
+        }
+
+        return result.toString();
     }
 }
---
+++ src/test/java/com/tszalama/shorturlapi/integration/ShortUrlApiIntegrationTests.java
@@ -0,0 +1,65 @@
+package com.tszalama.shorturlapi.integration;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tszalama.shorturlapi.dto.ShortUrlCreationRequestDTO;
+import com.tszalama.shorturlapi.dto.ShortUrlCreationResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.RepeatedTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Slf4j
+public class ShortUrlApiIntegrationTests {
+
+    //TODO: Use separate test database instance for testing
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private int testIteration = 0;
+    private final String baseTestUrl = "https://www.google.com/search?q=what+is+2+%2B";
+
+    @RepeatedTest(100)
+    public void createShortenedUrlAndUseIt() throws Exception {
+        //get unique url for each iteration
+        String testUrl = baseTestUrl + testIteration;
+
+        //validate urlId creation
+        ShortUrlCreationRequestDTO shortUrlRequestDTO = new ShortUrlCreationRequestDTO(testUrl);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(shortUrlRequestDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.urlId").exists())
+                .andReturn();
+
+        //validate retrieval based on generated url
+        String jsonResponse = result.getResponse().getContentAsString();
+        JsonNode jsonNode = objectMapper.readTree(jsonResponse);
+        String createdUrlId = jsonNode.get("urlId").asText();
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/" + createdUrlId))
+                .andExpect(status().isSeeOther())
+                .andExpect(MockMvcResultMatchers.header().string(HttpHeaders.LOCATION, testUrl));
+
+        log.info("Url with id " + createdUrlId + " shortened and validated");
+
+        testIteration++;
+    }
+}
